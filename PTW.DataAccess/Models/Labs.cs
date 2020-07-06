cd56565903e036aecd6f3ab72337af1610737813
Mateen Shaik <mateen.shaik@ptw.com>
2020-07-06 20:00:18 +0530

    Initial Lab CMS commited

--- PTW.DataAccess/Models/Labs.cs
+++ PTW.DataAccess/Models/Labs.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Org.BouncyCastle.Asn1.Mozilla;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
 using System.Text;
 
 namespace PTW.DataAccess.Models
@@ -8,8 +12,14 @@ namespace PTW.DataAccess.Models
     {
         public int LabId { get; set; }
         public int ServiceTypeId { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Short Description is required")]
         public string ShortDescription { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
         public string ImageName { get; set; }
         public string ImageUrl { get; set; }
@@ -32,18 +42,18 @@ namespace PTW.DataAccess.Models
         public string Reason { get; set; }
         public int ShortOrder { get; set; }
         public int IsActive { get; set; }
+
+        [Required(ErrorMessage = "Lab Type is required")]
         public string LabType { get; set; }
+        public int LabTypeId { get; set; }
         public string ImageAlternateText { get; set; }
         public string LanguageCode { get; set; }
         public int SliderId { get; set; }
-
         public string DeskL_ImageURL { get; set; }
-
         public string Desk_ImageURL { get; set; }
         public string TabH_ImageURL { get; set; }
         public string TabV_ImageURL { get; set; }
         public string Mobile_ImageURL { get; set; }
-       
         public string CEandQAImageUrl { get; set; }
         public string CustomerExperienceImageUrl { get; set; }
         public string QualityAssuranceImageUrl { get; set; }
@@ -52,36 +62,70 @@ namespace PTW.DataAccess.Models
         public string EngineeringImageUrl { get; set; }
         public string CampaignImageUrl { get; set; }
         public string LatestInsightImageUrl { get; set; }
+        public string Image { get; set; }
         public int Stopped { get; set; }
-
         public string DetailedDescription { get; set; }
-
         public string DetailedName { get; set; }
         public string DetailedShortOrder { get; set; }
         public string ReadMoreUrl { get; set; }
-
         public string FilterCustomerExperience { get; set; }
         public string FilterQualityAssurance { get; set; }
         public string FilterLocalization { get; set; }
         public string FilterAudioProduction { get; set; }
         public string FilterEngineering { get; set; }
         public string FilterCampaign { get; set; }
+        [Required(ErrorMessage = "Service Type is required")]
+        public string ServiceType { get; set; }
+        public string StartDate { get; set; }//current date
+        public string EndDate { get; set; }//sleected
+        public string DesktopName { get; set; }
+
+        public string MobileName { get; set; }
+
+        public string TabImgHorizonalname { get; set; }
+
+        public string TabImgVerticalname { get; set; }
+
+        public string ReadMorename { get; set; }
+
+        public string MetaTitle { get; set; }
+
+        public string MetaDescription { get; set; }
+
+        [Required(ErrorMessage = "Please upload image and size should be 376x360 dimensions")]
+        public IFormFile MobileImage { get; set; }
+
+        [Required(ErrorMessage = "Please upload image and size should be 1920x750 dimensions")]
+        public IFormFile DesktopImage { get; set; }
+
+        [Required(ErrorMessage = "Please upload image and size should be 1960x400 dimensions")]
+        public IFormFile TabImageHorizonal { get; set; }
+
+        [Required(ErrorMessage = "Please upload image and size should be 800x350 dimensions")]
+        public IFormFile TabImageNamVertical { get; set; }
+
+        [Required(ErrorMessage = "Please upload image and size should be 320x220 dimensions")]
+        public IFormFile ReadMore { get; set; }
+
+       
+
+        public bool? ActiveStatus { get; set; }
+
         public List<Languages> Languages { get; set; }
-        public List<Labs> LabsList { get; set; }       
+        public List<Labs> LabsList { get; set; }
     }
 
-    
+
     public class LabsArticles
     {
 
-        public int LabId { get; set; }
-        public int ServiceTypeId { get; set; }
-        public string Name { get; set; }
-        public string ShortDescription { get; set; }
-        public string Description { get; set; }
-        public string ImageName { get; set; }
-        public string ImageUrl { get; set; }
-        public string Topic { get; set; }
+        public int ServiceType { get; set; } //dropdown
+        public string Name { get; set; }// testbpx
+        public string Description { get; set; }//texteditor
+        public string ShortDescription { get; set; } //textbox
+        public string ImageName { get; set; } //insert no need to show on browser
+        public string ImageUrl { get; set; } //insert no need to show on browser
+        public string Topic { get; set; } //textbox //not mandatory
         public bool? OnNewWebsiteNow { get; set; }
         public bool? SuitedForHomePage { get; set; }
         public bool? CustomerExperience { get; set; }
@@ -90,50 +134,98 @@ namespace PTW.DataAccess.Models
         public bool? AudioProduction { get; set; }
         public bool? Engineering { get; set; }
         public bool? Campaign { get; set; }
-        public int Event { get; set; }
         public string MetaTags { get; set; }
-        public string DesktopImageUrl { get; set; }
+        [Required(ErrorMessage = "Please upload image and size should be 960x400 dimensions")]
+        public IFormFile DesktopImageUrl { get; set; }
+
+        public IFormFile DesktopImage { get; set; }
+
+
+       [Required(ErrorMessage = "Please upload image and size should be 960x800 dimensions")]
         public string TabImageNameHorizondaUrl { get; set; }
+
+        public IFormFile TabImageHorizonal { get; set; }
+
+        [Required(ErrorMessage = "Please upload image and size should be 1520x445 dimensions")]
         public string TabImageNamVerticalUrl { get; set; }
+
+        public IFormFile TabImageNamVertical { get; set; }
         public string MobileImageNameUrl { get; set; }
-        public string DefaultImageUrl { get; set; }
-        public string Reason { get; set; }
-        public int ShortOrder { get; set; }
-        public int IsActive { get; set; }
-        public string LabType { get; set; }
-        public string ImageAlternateText { get; set; }
-        public string LanguageCode { get; set; }
-        public int SliderId { get; set; }
 
-        public string DeskL_ImageURL { get; set; }
-
-        public string Desk_ImageURL { get; set; }
-        public string TabH_ImageURL { get; set; }
-        public string TabV_ImageURL { get; set; }
-        public string Mobile_ImageURL { get; set; }
-
-        public string CEandQAImageUrl { get; set; }
-        public string CustomerExperienceImageUrl { get; set; }
-        public string QualityAssuranceImageUrl { get; set; }
-        public string LocalizationImageUrl { get; set; }
-        public string AudioProductionImageUrl { get; set; }
-        public string EngineeringImageUrl { get; set; }
-        public string CampaignImageUrl { get; set; }
-        public string LatestInsightImageUrl { get; set; }
-        public int Stopped { get; set; }
-
-        public string DetailedDescription { get; set; }
-
-        public string DetailedName { get; set; }
-        public string DetailedShortOrder { get; set; }
+        public IFormFile MobileImage { get; set; }
         public string ReadMoreUrl { get; set; }
 
-        public string FilterCustomerExperience { get; set; }
-        public string FilterQualityAssurance { get; set; }
-        public string FilterLocalization { get; set; }
-        public string FilterAudioProduction { get; set; }
-        public string FilterEngineering { get; set; }
-        public string FilterCampaign { get; set; }
+        public IFormFile ReadMore { get; set; }
+
+        [Required(ErrorMessage = "Please upload image and size should be 1520x445 dimensions")]
+        public IFormFile Image { get; set; }
+        public IFormFile LatestInsightImageUrl { get; set; }
+        public string DefaultImageUrl { get; set; }
+        public string IsLatestInsight { get; set; }
+        public string StartDate { get; set; }//current date
+        public string EndDate { get; set; }//sleected
+        public string ShortOrder { get; set; }//no npublic stringeed to show on brouwserwser
+        public string LabType { get; set; }
+        public string IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public string metatitle { get; set; }
+        public string metadescription { get; set; }
+        
+        public int LabTypeId { get; set; }
     }
 
-}
+    public class EditLabs
+    {
+        public int ServiceType { get; set; } //dropdown
+        public string Name { get; set; }// testbpx
+        public string Description { get; set; }//texteditor
+        public string ShortDescription { get; set; } //textbox
+        public string ImageName { get; set; } //insert no need to show on browser
+        public string ImageUrl { get; set; } //insert no need to show on browser
+        public string Topic { get; set; } //textbox //not mandatory
+        public bool? OnNewWebsiteNow { get; set; }
+        public bool? SuitedForHomePage { get; set; }
+        public bool? CustomerExperience { get; set; }
+        public bool? QualityAssurance { get; set; }
+        public bool? Localization { get; set; }
+        public bool? AudioProduction { get; set; }
+        public bool? Engineering { get; set; }
+        public bool? Campaign { get; set; }
+        public string MetaTags { get; set; }
+        [Required(ErrorMessage = "Please upload image and size should be 960x400 dimensions")]
+        public IFormFile DesktopImageUrl { get; set; }
+        [Required(ErrorMessage = "Please upload image and size should be 960x800 dimensions")]
+        public string TabImageNameHorizondaUrl { get; set; }
+        [Required(ErrorMessage = "Please upload image and size should be 1520x445 dimensions")]
+        public string TabImageNamVerticalUrl { get; set; }
+        public string MobileImageNameUrl { get; set; }
+        public string ReadMoreUrl { get; set; }
+        public string LatestInsightImageUrl { get; set; }
+        public string DefaultImageUrl { get; set; }
+
+        [Required(ErrorMessage = "Please upload image and size should be 1520x445 dimensions")]
+        public IFormFile Image { get; set; }
+        public string IsLatestInsight { get; set; }
+        public string StartDate { get; set; }//current date
+        public string EndDate { get; set; }//sleected
+        public string ShortOrder { get; set; }//no npublic stringeed to show on brouwserwser
+        public string LabType { get; set; }
+        public string IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public string metatitle { get; set; }
+        public string metadescription { get; set; }
+        public string LabTypeId { get; set; }
+
+        public IFormFile MobileImage { get; set; }
+
+        public IFormFile DesktopImage { get; set; }
+        public IFormFile TabImageHorizonal { get; set; }
+
+        public IFormFile TabImageNamVertical { get; set; }
+
+        public IFormFile ReadMore { get; set; }
+
+        //public int LabTypeId { get; set; }
+    }
+
+    }
--- PTWWebsite2/Controllers/LabController.cs
+++ PTWWebsite2/Controllers/LabController.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Design.Internal;
 using PTW.DataAccess.Models;
 using PTW.DataAccess.Services;
 
@@ -52,7 +53,7 @@ namespace PTWWebsite2.Controllers
         }
         [Route("LAB/{LabShortDescription}")]
         [Route("{culture}/LAB/{LabShortDescription}")]
-        public IActionResult LabArticles(string LabShortDescription,string culture = "en-US")        
+        public IActionResult LabArticles(string LabShortDescription,string culture = "en-US")
         {
             List<string> ShortDescription = LabShortDescription.Split('-').ToList();
             ShortDescription.RemoveAt(0);
@@ -69,8 +70,7 @@ namespace PTWWebsite2.Controllers
                 LabsEvents.LabArticledetails = _LabEventService.GetLabsArticleDetails(LabShortDescription);
                 LabsEvents.FutureLabArticles = _LabEventService.GetFutureLabArticles(LabShortDescription);
 
-            }
-            
+            }            
             GetServicecontent(culture);
             return View(LabsEvents);
         }
@@ -86,85 +86,122 @@ namespace PTWWebsite2.Controllers
             return View(labs);
         }
 
-        [Route("Labs/{LabTitleUrl}")]
+
+        [Route("LAB/{LabShortDescription}")]
         [HttpPost]
-        public IActionResult AddUpdateNews(News news)
+        public IActionResult AddUpdateLabs(Labs labs)
         {
-           
-            if (ModelState.IsValid)
-            {
+
+            //if (ModelState.IsValid)
+            //{
 
                 string uniqueFileName = string.Empty;
-                string headerImageFolder = string.Empty;
-                string LorgeImageFolder = string.Empty;
-                string smallImageFolder = string.Empty;
-
-                if (news.HeaderImage != null)
+                string MobileImage = string.Empty;
+                string DesktopImage = string.Empty;
+                string TabImageHorizental = string.Empty;
+                string TabImageVertical = string.Empty;
+                string Readmore = string.Empty;
+                string Labtypeimg = string.Empty;
+                string Mobileurl = string.Empty;
+                if (labs.DesktopImage != null)
                 {
-                    headerImageFolder = Path.Combine(_hostingEnvironment.WebRootPath, "images/News/2020/Header");
-                    LorgeImageFolder = Path.Combine(_hostingEnvironment.WebRootPath, "images/News/2020/Large");
-                    smallImageFolder = Path.Combine(_hostingEnvironment.WebRootPath, "images/News/2020/Small");
+                    if (labs.LabTypeId == 1)
+                    {
+                        Labtypeimg = "Articles";
 
-                    news.HeaderImageUrl = "/Images/News/2020/Header/";
-                    news.HeaderImageName = news.HeaderImage.FileName;
-                    string filePath = Path.Combine(headerImageFolder, news.HeaderImage.FileName);
-                    news.HeaderImage.CopyTo(new FileStream(filePath, FileMode.Create));
+                    }
+                    else
+                    {
+                        Labtypeimg = "CaseStudies";
 
-                    news.LongImageUrl = "/Images/News/2020/Large/";
-                    news.LongImageName = news.LongerImage.FileName;
-                    string filePath2 = Path.Combine(LorgeImageFolder, news.LongerImage.FileName);
-                    news.HeaderImage.CopyTo(new FileStream(filePath2, FileMode.Create));
+                    }
 
-                    news.ShortImageUrl = "/Images/News/2020/Small/";
-                    news.ShortImageName = news.ShorterImage.FileName;
-                    string filePath3 = Path.Combine(smallImageFolder, news.ShorterImage.FileName);
-                    news.HeaderImage.CopyTo(new FileStream(filePath3, FileMode.Create));
+                    MobileImage = Path.Combine(_hostingEnvironment.WebRootPath, "images/Lab/" + Labtypeimg + "/376");
+                    DesktopImage = Path.Combine(_hostingEnvironment.WebRootPath, "images/Lab/" + Labtypeimg + "/1920");
+                    TabImageHorizental = Path.Combine(_hostingEnvironment.WebRootPath, "images/Lab/" + Labtypeimg + "/1112");
+                    TabImageVertical = Path.Combine(_hostingEnvironment.WebRootPath, "images/Lab/" + Labtypeimg + "/834");
+                    Readmore = Path.Combine(_hostingEnvironment.WebRootPath, "images/Lab/" + Labtypeimg + "/320");
+                
+                    labs.ImageUrl = "/Images/Lab/" + Labtypeimg + "/376/";
+                    labs.ImageName = labs.MobileImage.FileName;
 
+                    labs.MobileImageNameUrl = "/Images/Lab/" + Labtypeimg + "/376/"+ labs.MobileImage.FileName + "";
+                    labs.MobileName = labs.MobileImage.FileName;                 
+
+                    string filePath = Path.Combine(MobileImage, labs.MobileImage.FileName);
+                    labs.MobileImage.CopyTo(new FileStream(filePath, FileMode.Create));
+
+
+                    labs.DesktopImageUrl = "/Images/Lab/" + Labtypeimg + "/1920/" + labs.DesktopImage.FileName + "";
+                    labs.DesktopName = labs.DesktopImage.FileName;
+                    string filePath2 = Path.Combine(DesktopImage, labs.DesktopImage.FileName);
+                    labs.DesktopImage.CopyTo(new FileStream(filePath2, FileMode.Create));
+
+                    labs.TabImageNameHorizondaUrl = "/Images/Lab/" + Labtypeimg + "/1112/" + labs.TabImageHorizonal.FileName + "";
+                    labs.TabImgHorizonalname = labs.TabImageHorizonal.FileName;
+                    string filePath3 = Path.Combine(TabImageHorizental, labs.TabImageHorizonal.FileName);
+                    labs.TabImageHorizonal.CopyTo(new FileStream(filePath3, FileMode.Create));
+
+                    labs.TabImageNamVerticalUrl = "/Images/Lab/" + Labtypeimg + "/834/" + labs.TabImageNamVertical.FileName + "";
+                    labs.TabImgVerticalname = labs.TabImageNamVertical.FileName;
+                    string filePath4 = Path.Combine(TabImageVertical, labs.TabImageNamVertical.FileName);
+                    labs.TabImageNamVertical.CopyTo(new FileStream(filePath4, FileMode.Create));
+
+                    labs.ReadMoreUrl = "/Images/Lab/" + Labtypeimg + "/320/" + labs.ReadMore.FileName + "";
+                    labs.ReadMorename = labs.ReadMore.FileName;
+                    string filePath5 = Path.Combine(Readmore, labs.ReadMore.FileName);
+                    labs.ReadMore.CopyTo(new FileStream(filePath5, FileMode.Create));
                 }
 
-                string newsXmlData = CustomNewsXml(news);
-                bool result = _LabEventService.AddUpdateLabs(newsXmlData, news.Description);
+                string labsXmlData = CustomNewsXml(labs);
+                bool result = _LabEventService.AddUpdateLabs(labsXmlData, labs.Description);
                 ViewBag.IsAddedSuccessfully = result;
                 ModelState.Clear();
-            }
+            //}
 
-            return View("AddNewsEvents");
+            return View("AddLabArticles",labs);
         }
 
         [NonAction]
-        public string CustomNewsXml(News news)
+        public string CustomNewsXml(Labs labs)
         {
             StringBuilder xml = new StringBuilder();
             xml.Append("<Labs>");
-            if (news != null)
+            if (labs != null)
             {
-               
-                //xml.Append(string.Format("<Description>{0}</Description>", news.Description));
-                xml.Append(string.Format("<Title>{0}</Title>", news.NewsTitle));
-                xml.Append(string.Format("<ShortDescription>{0}</ShortDescription>", news.UrlTitle.Replace("-", " ")));
-                xml.Append(string.Format("<Topic>{0}</Topic>", news.Topic));
-                xml.Append(string.Format("<OnNewWebsiteNow>{0}</OnNewWebsiteNow>", Convert.ToInt32(news.OnNewWebsiteNow)));
-                xml.Append(string.Format("<SuitedForHomePage>{0}</SuitedForHomePage>", Convert.ToInt32(news.SuitedForHomePage)));
-                xml.Append(string.Format("<CustomerExperience>{0}</CustomerExperience>", Convert.ToInt32(news.CustomerExperience)));
-                xml.Append(string.Format("<QualityAssurance>{0}</QualityAssurance>", Convert.ToInt32(news.QualityAssurance)));
-                xml.Append(string.Format("<Localization>{0}</Localization>", Convert.ToInt32(news.Localization)));
-                xml.Append(string.Format("<AudioProduction>{0}</AudioProduction>", Convert.ToInt32(news.AudioProduction)));
-                xml.Append(string.Format("<Engineering>{0}</Engineering>", Convert.ToInt32(news.Engineering)));
-                xml.Append(string.Format("<Event>{0}</Event>", news.Event));
-                xml.Append(string.Format("<HeaderImageName>{0}</HeaderImageName>", news.HeaderImageName));
-                xml.Append(string.Format("<HeaderImageUrl>{0}</HeaderImageUrl>", news.HeaderImageUrl));
-                xml.Append(string.Format("<LongImageName>{0}</LongImageName>", news.LongImageName));
-                xml.Append(string.Format("<LongImageUrl>{0}</LongImageUrl>", news.LongImageUrl));
-                xml.Append(string.Format("<ShortImageName>{0}</ShortImageName>", news.ShortImageName));
-                xml.Append(string.Format("<ShortImageUrl>{0}</ShortImageUrl>", news.ShortImageUrl));
-                xml.Append(string.Format("<ShortOrder>{0}</ShortOrder>", news.ShortOrder));
-                xml.Append(string.Format("<EventStartDate>{0}</EventStartDate>", news.EventStartDate != "" ? Convert.ToDateTime(news.EventStartDate) : DateTime.Now));
-                xml.Append(string.Format("<EventEndDate>{0}</EventEndDate>", news.EventEndDate != "" ? Convert.ToDateTime(news.EventStartDate) : DateTime.Now));
-                xml.Append(string.Format("<IsActive>{0}</IsActive>", Convert.ToInt32(news.ActiveStatus)));
-                xml.Append(string.Format("<CreatedBy>{0}</CreatedBy>", 1));
-                xml.Append(string.Format("<MetaTitle>{0}</MetaTitle>", news.MetaTitle));
-                xml.Append(string.Format("<MetaDescription>{0}</MetaDescription>", news.MetaDescription));
 
+                xml.Append(string.Format("<ServiceTypeId>{0}</ServiceTypeId>", labs.ServiceTypeId));
+                xml.Append(string.Format("<Name>{0}</Name>", labs.Name));
+                xml.Append(string.Format("<Description>{0}</Description>", labs.Description));
+                xml.Append(string.Format("<ShortDescription>{0}</ShortDescription>", labs.ShortDescription.Replace("-", " ")));
+                xml.Append(string.Format("<ImageName>{0}</ImageName>", labs.ImageName));
+                xml.Append(string.Format("<ImageUrl>{0}</ImageUrl>", labs.ImageUrl));
+                xml.Append(string.Format("<Topic>{0}</Topic>", labs.Topic));
+
+                xml.Append(string.Format("<OnNewWebsiteNow>{0}</OnNewWebsiteNow>", Convert.ToInt32(labs.OnNewWebsiteNow)));
+                xml.Append(string.Format("<SuitedForHomePage>{0}</SuitedForHomePage>", Convert.ToInt32(labs.SuitedForHomePage)));
+                xml.Append(string.Format("<CustomerExperience>{0}</CustomerExperience>", Convert.ToInt32(labs.CustomerExperience)));
+                xml.Append(string.Format("<QualityAssurance>{0}</QualityAssurance>", Convert.ToInt32(labs.QualityAssurance)));
+                xml.Append(string.Format("<Localization>{0}</Localization>", Convert.ToInt32(labs.Localization)));
+                xml.Append(string.Format("<AudioProduction>{0}</AudioProduction>", Convert.ToInt32(labs.AudioProduction)));
+                xml.Append(string.Format("<Engineering>{0}</Engineering>", Convert.ToInt32(labs.Engineering))); 
+                xml.Append(string.Format("<Campaign>{0}</Campaign>", Convert.ToInt32(labs.Campaign)));
+
+                xml.Append(string.Format("<DesktopImageUrl>{0}</DesktopImageUrl>", labs.DesktopImageUrl));
+                xml.Append(string.Format("<TabImageNameHorizondaUrl>{0}</TabImageNameHorizondaUrl>", labs.TabImageNameHorizondaUrl));
+                xml.Append(string.Format("<TabImageNamVerticalUrl>{0}</TabImageNamVerticalUrl>", labs.TabImageNamVerticalUrl));
+                xml.Append(string.Format("<MobileImageNameUrl>{0}</MobileImageNameUrl>", labs.MobileImageNameUrl));
+                xml.Append(string.Format("<ReadMoreUrl>{0}</ReadMoreUrl>", labs.ReadMoreUrl));
+
+                xml.Append(string.Format("<ShortOrder>{0}</ShortOrder>", labs.ShortOrder));
+                xml.Append(string.Format("<LabType>{0}</LabType>", labs.LabType));
+                xml.Append(string.Format("<StartDate>{0}</StartDate>", labs.StartDate != "" ? Convert.ToDateTime(labs.StartDate) : DateTime.Now));
+                xml.Append(string.Format("<EndDate>{0}</EndDate>", labs.EndDate != "" ? Convert.ToDateTime(labs.StartDate) : DateTime.Now));
+               
+                xml.Append(string.Format("<MetaTitle>{0}</MetaTitle>", labs.MetaTitle));
+                xml.Append(string.Format("<MetaDescription>{0}</MetaDescription>", labs.MetaDescription));
+                xml.Append(string.Format("<IsActive>{0}</IsActive>", Convert.ToInt32(labs.ActiveStatus)));
+                xml.Append(string.Format("<CreatedBy>{0}</CreatedBy>", 1));
             }
             xml.Append("</Labs>");
             return xml.ToString();
